dd6b6faf6c1fa4ad739647a21c9fb8298a633c88
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2021-06-07 19:48:53 +0300

    Initial Commit

---
+++ WeatherApp_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp_2
+//
+//  Created by Никита Галкин on 07.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp_2/WeatherApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_2App.swift
+//  WeatherApp_2
+//
+//  Created by Никита Галкин on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
